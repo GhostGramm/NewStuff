694b2593fa9824a810acc40b61d54e323df7db56
Marshal Jr <marshalonah@gmail.com>
2021-08-25 09:23:17 +0100

    collision bug fix

--- assets/script/GameManager.ts
+++ assets/script/GameManager.ts
@@ -1,28 +1,39 @@
 
-import { _decorator, Component, Node, Prefab } from 'cc';
+import { _decorator, Component, Node, Prefab, instantiate, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameManager')
 export class GameManager extends Component {
 
     @property(Node)
-    player : Node = null;
+    player: Node = null;
     @property(Prefab)
-    star : Prefab = null;
+    star: Prefab = null;
     @property(Node)
-    ground : Node = null;
+    ground: Node = null;
 
     @property
-    minStarDuration : number;
+    minStarDuration: number = 0;
     @property
-    maxStarDuration : number;
-    
+    maxStarDuration: number = 0;
+    groundY : any;
 
-    start () {
-        
+    spawnStar() {
+        var starIns = instantiate(this.star);
+        this.node.addChild(starIns);
+        starIns.setPosition(this.spawnPosition())
     }
 
-    update (deltaTime: number) {
-        
+    spawnPosition(){
+        return new Vec3(0,0,0);
+    }
+
+    start() {
+        this.groundY = this.ground.position.x * this.ground.position.y/2;
+        console.log(this.ground.position.y);
+    }
+
+    update(deltaTime: number) {
+
     }
 }
